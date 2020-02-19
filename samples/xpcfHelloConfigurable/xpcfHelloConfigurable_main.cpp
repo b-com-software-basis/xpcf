2367e5a9e67d2fa373d6822cb7b3af3555047643
Firefly35 <loic.touraine@gmail.com>
2020-02-19 15:49:45 +0100

    fix[xpcfHelloConfigurable] : add missing file

---
+++ samples/xpcfHelloConfigurable/xpcfHelloConfigurable_main.cpp
@@ -0,0 +1,35 @@
+
+#include <xpcf/module/ModuleFactory.h>
+#include <iostream>
+#include "ConfigurableComponent.h"
+
+namespace xpcf=org::bcom::xpcf;
+
+/**
+ *  @ingroup xpcfmodule
+ */
+/**
+  * Declare module.
+  */
+XPCF_DECLARE_MODULE("F2C2D134-E627-449E-893C-D11BBBF47C9E","xpcfHelloConfigurable","xpcfHelloConfigurable module description");
+
+/**
+ * This method is the module entry point.
+ * XPCF uses this method to create components available in the module.
+ *
+ * Each component exposed must be declared inside a xpcf::tryCreateComponent<ComponentType>() call.
+ */
+extern "C" XPCF_MODULEHOOKS_API xpcf::XPCFErrorCode XPCF_getComponent(const xpcf::uuids::uuid& componentUUID,SRef<xpcf::IComponentIntrospect>& interfaceRef)
+{
+    xpcf::XPCFErrorCode errCode = xpcf::XPCFErrorCode::_FAIL;
+    errCode = xpcf::tryCreateComponent<ConfigurableComponent>(componentUUID,interfaceRef);
+    return errCode;
+}
+
+/**
+  * The declarations below populate list of the components available in the module (it represents the module index).
+  * XPCF uses this index to introspect the components available in a module, providing the ability to generate the configuration file skeleton from the code.
+  */
+XPCF_BEGIN_COMPONENTS_DECLARATION
+XPCF_ADD_COMPONENT(ConfigurableComponent)
+XPCF_END_COMPONENTS_DECLARATION
