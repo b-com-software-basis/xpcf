63b10f9e44e6fdd172e350df58e3381cf1a84e49
Firefly35 <loic.touraine@gmail.com>
2023-03-20 14:13:42 +0100

    fix: put xpcfErrorCodeToMessageMap in Exception.cpp

--- src/BaseException.cpp
+++ src/BaseException.cpp
@@ -4,35 +4,6 @@
 #include "xpcf/core/BaseException.h"
 namespace org { namespace bcom { namespace xpcf {
 
-std::map<XPCFErrorCode, std::string> xpcfErrorCodeToMessageMap = {
-    { XPCFErrorCode::_SUCCESS , "SUCCESS" },
-    { XPCFErrorCode::_FAIL , "FAIL" },
-    { XPCFErrorCode::_ERROR_INVALID_ARGUMENT , "ERROR: argument provided is invalid" },
-    { XPCFErrorCode::_ERROR_DOMAIN , "ERROR: domain error " },
-    { XPCFErrorCode::_ERROR_NOT_IMPLEMENTED , "ERROR: method is not implemented yet" },
-    { XPCFErrorCode::_ERROR_NULL_POINTER , "ERROR: Null pointer argument provided" },
-    { XPCFErrorCode::_ERROR_OUT_OF_RANGE , "ERROR: Out of range argument" },
-    { XPCFErrorCode::_ERROR_TYPE , "ERROR: Unsupported type" },
-    { XPCFErrorCode::_ERROR_OVERFLOW , "ERROR: overflow" },
-    { XPCFErrorCode::_ERROR_UNDERFLOW , "ERROR: underflow" },
-    { XPCFErrorCode::_ERROR_OUT_OF_MEMORY , "ERROR: out of memory" },
-    { XPCFErrorCode::_ERROR_RANGE , "ERROR: range" },
-    { XPCFErrorCode::_ERROR_SYSTEM , "ERROR: system" },
-    { XPCFErrorCode::_ERROR_UNKNOWN , "ERROR: unknown" },
-    { XPCFErrorCode::_ERROR_ACCESS_DENIED , "ERROR: access denied " },
-    { XPCFErrorCode::_ERROR_TIMEOUT , "ERROR: timeout " },
-    { XPCFErrorCode::_ERROR_ILLEGAL_STATE, "ERROR: illegal state " },
-    { XPCFErrorCode::_ERROR_COMPONENT_UNKNOWN , "ERROR: unknown component " },
-    { XPCFErrorCode::_ERROR_INTERFACE_UNKNOWN , "ERROR: unknown interface" },
-    { XPCFErrorCode::_ERROR_MODULE_UNKNOWN , "ERROR: unknown module" },
-    { XPCFErrorCode::_ERROR_MODULE_MISSINGXPCF_ENTRY , "ERROR: missing xpcf method(s) in the library (either one or several from getModuleUUID, getModuleName, getComponent ... )" },
-    { XPCFErrorCode::_ERROR_INJECTION_FAILED , "ERROR: injection failed" },
-    { XPCFErrorCode::_ERROR_INJECTABLE_UNKNOWN , "ERROR: injectable unknown" },
-    { XPCFErrorCode::_ERROR_INJECTABLE_ALREADY_DECLARED , "ERROR: injectable already declared" },
-    { XPCFErrorCode::_ERROR_INJECTABLE_NOBIND , "ERROR: no binding declared for injectable" },
-};
-
-
 Exception::Exception(XPCFErrorCode errCode):
     std::runtime_error(xpcfErrorCodeToMessageMap[errCode]),m_errCode(errCode)
 {
