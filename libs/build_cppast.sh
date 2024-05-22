b29c65c37898b09886bc466ce88a4b42be633864
aschadle <adrien.schadle@b-com.com>
2024-05-22 12:21:07 +0200

    fix(ci): remove useless brew for linux build of cppast

--- libs/build_cppast.sh
+++ libs/build_cppast.sh
@@ -19,17 +19,9 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
         LLVMVERSION=14
     fi
     sudo apt-get install -y llvm-${LLVMVERSION} clang-${LLVMVERSION} libclang-${LLVMVERSION}-dev
-    LLVMBREWPATH=/home/linuxbrew/.linuxbrew
     LLVMCONFIGPATH=/usr/bin/llvm-config-${LLVMVERSION}
     TARGETPLATFORM=linux-gcc
     CPPASTCMAKECOMPILER="-DCMAKE_CXX_COMPILER=clang++-${LLVMVERSION}"
-    if test ! $(which brew); then
-	echo "brew is not installed, first install brew then relaunch this script\n";
-	exit 1
-    fi
-    # echo "installing llvm"
-#    brew install llvm
-    # ...
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # Mac OSX
     if test ! $(which brew); then
